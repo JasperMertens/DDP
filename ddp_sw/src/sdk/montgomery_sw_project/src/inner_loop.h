5ec3fadaa412602fa532f89ab25be9a8dba80dd1
Jasper Mertens <r0575928@pc-klas1-10.esat.kuleuven.be>
2018-08-01 17:53:00 +0200

    gitignore update7

---
+++ ddp_sw/src/sdk/montgomery_sw_project/src/inner_loop.h
@@ -0,0 +1,15 @@
+/*
+ * asm_func.h
+ *
+ *  Created on: May 13, 2016
+ *      Author: dbozilov
+ */
+
+#ifndef INNER_LOOP_H_
+#define INNER_LOOP_H_
+
+#include <stdint.h>
+
+void inner_loop(uint32_t *C, uint32_t *S, uint32_t *t, uint32_t *a, uint32_t *b, uint32_t m, uint32_t *n, uint32_t i, uint32_t j);
+
+#endif /* INNER_LOOP_H_ */
---
+++ ddp_sw/src/sdk/montgomery_sw_project/src/inner_loop3.c
@@ -0,0 +1,8 @@
+/*
+ * inner_loop3.c
+ *
+ *  Created on: Oct 28, 2017
+ *      Author: r0388391
+ */
+
+
