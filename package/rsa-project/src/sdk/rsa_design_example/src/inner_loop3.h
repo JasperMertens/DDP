966b3761d6751ddd4c9b54ba9bbdd60f73a8d039
Jasper Mertens <r0575928@pc-klas1-10.esat.kuleuven.be>
2018-08-02 10:52:19 +0200

    juiste c files toegevoegd

---
+++ package/rsa-project/src/sdk/rsa_design_example/src/inner_loop3.h
@@ -0,0 +1,12 @@
+/*
+ * asm_func.h
+ */
+
+#ifndef INNER_LOOP3_H_
+#define INNER_LOOP3_H_
+
+#include <stdint.h>
+
+void inner_loop3(uint32_t *C, uint32_t i, uint32_t *t, uint32_t *a, uint32_t *b, uint32_t m, uint32_t *n);
+
+#endif /* INNER_LOOP3_H_ */
