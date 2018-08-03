7466148845687c68c7c6e943bbe6869acd1df3c2
Koen Geerts <r0594518@pc-klas1-3.esat.kuleuven.be>
2018-08-03 16:27:34 +0200

    mont exp software working biatch

--- package/rsa-project/src/sdk/rsa_design_example/src/sw.h
+++ package/rsa-project/src/sdk/rsa_design_example/src/sw.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 
 // Calculates res = (message^exponent) mod N
-void mod_exp(uint32_t *msg, uint32_t *exp, uint32_t exp_len, uint32_t *n, uint32_t *n_prime, uint32_t *res);
+void mod_exp(uint32_t *msg, uint32_t *exp, uint32_t exp_len, uint32_t *n, uint32_t *n_prime, uint32_t *R, uint32_t *R2, uint32_t *res);
 
 // Calculates res = (a * b / R) mod N where R = 2^1024
 void montgomery_multiply(uint32_t *a, uint32_t *b, uint32_t *n, uint32_t *n_prime, uint32_t *res, uint32_t size);
