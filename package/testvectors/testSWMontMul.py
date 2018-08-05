05c1f2758796a97d7dd1a6f8ff69808ce0702583
Helen Hochstrasser <r0388391@pc-klas1-4.esat.kuleuven.be>
2018-08-05 14:46:00 +0200

    montMul testvectors

--- package/testvectors/testSWMontMul.py
+++ package/testvectors/testSWMontMul.py
@@ -2,21 +2,22 @@ import helpers
 
 print "Test Vector for Montgomery Multiplication\n"
 
-a = helpers.getRandomInt(1024)
-b = helpers.getRandomInt(1024)
 n = helpers.getModulus(1024)
+a = helpers.getRandomInt(1024) % n
+b = helpers.getRandomInt(1024) % n
+
 r = 2**1024                      
-n_prime = (r - Modinv(n, r))	
+n_prime = (r - helpers.Modinv(n, r))	
 expected = (a*b*helpers.Modinv(r,n)) % n
 
-print "A                = ", hex(A)         
-print "B                = ", hex(B)         
-print "M                = ", hex(M)        
-print "(A*B*R^-1) mod M = ", hex(C)        
-
+#print "a                = ", hex(a)         
+#print "b                = ", hex(b)         
+#print "n                = ", hex(n)        
+#print "(a*b*r^-1) mod n = ", hex(expected)        
+print
 print "uint32_t a[32] = {" + helpers.WriteConstants(a,32) + "};"
 print "uint32_t b[32] = {" + helpers.WriteConstants(b,32) + "};"   
-print "uint32_t n[32] = {" + helpers.WriteConstants(n,32) + "};"
+print "uint32_t N[32] = {" + helpers.WriteConstants(n,32) + "};"
 print "uint32_t n_prime[32] = {" + helpers.WriteConstants(n_prime,32) + "};" 
 print "uint32_t expected_output[32] = {" + helpers.WriteConstants(expected,32) + "};" 
 
